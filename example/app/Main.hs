91678f105bfeaecd3b1bb274c8c0f412b3cb7e6d
adrianadrianadrianadrianadrian <a.glouftsis@outlook.com>
2023-03-28 22:42:42 +1100

    more of an eg?

--- example/app/Main.hs
+++ example/app/Main.hs
@@ -7,21 +7,28 @@ import           Control.Exception
 import           Control.Lens
 import           Data.Either
 import qualified Data.Text                     as T
+import qualified Data.ByteString               as BS
+import           Data.Maybe
 
 main :: IO ()
 main = do
     let context = defaultBlobContext "BLOB_ACC_NAME" "BLOB_ACC_KEY"
-    firstBlobDetails <- runBlobStorage context $ do
-        containerName <- printFirstContainerName
+    res <- runBlobStorage context $ do
+        let containerName = "example-container"
+        createContainer containerName
+        leaseId <- leaseContainer containerName Acquire 
+        putBlob containerName "example-blob" "Hello from example world"
+        printFirstBlobDetails
+        leaseContainer containerName (Release $ fromJust leaseId)
+    pure ()
+
+printFirstBlobDetails :: BlobStorage ()
+printFirstBlobDetails = do
+        containers <- listContainers
+        let containerName = head containers ^. name
+        withIO . print $ "First container: " <> containerName
+
         blobs <- listBlobs containerName
         let first = head blobs
         let props = first ^. blobProperties
-        pure $ "Name: " <> first ^. blobName <> "\nBytes " <> (T.pack . show $ props ^. contentByteLength)
-    print firstBlobDetails
-
-printFirstContainerName :: BlobStorage T.Text
-printFirstContainerName = do
-    containers <- listContainers
-    let containerName = head containers ^. name
-    withIO . print $ "First container: " <> containerName
-    pure containerName
+        withIO . print $ "Name: " <> first ^. blobName <> "\nBytes " <> (T.pack . show $ props ^. contentByteLength)
